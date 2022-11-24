6fffdb6ae9e282b207bfe015131c2faa06146ef0
Sayanaann <alexsayana@gmail.com>
2022-11-25 01:35:48 +0530

    all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LibraryappComponent } from './libraryapp/libraryapp.component';
+import { UserregComponent } from './userreg/userreg.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { BooksentryComponent } from './booksentry/booksentry.component';
+import { ViewallbooksComponent } from './viewallbooks/viewallbooks.component';
+import { BooksearchComponent } from './booksearch/booksearch.component';
+import { BookdeleteComponent } from './bookdelete/bookdelete.component';
+import { BookeditComponent } from './bookedit/bookedit.component';
+import { IssueBooksComponent } from './issue-books/issue-books.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LibraryappComponent
+    LibraryappComponent,
+    UserregComponent,
+    UserloginComponent,
+    BooksentryComponent,
+    ViewallbooksComponent,
+    BooksearchComponent,
+    BookdeleteComponent,
+    BookeditComponent,
+    IssueBooksComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/viewallbooks/viewallbooks.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewallbooks',
+  templateUrl: './viewallbooks.component.html',
+  styleUrls: ['./viewallbooks.component.css']
+})
+export class ViewallbooksComponent {
+
+}
